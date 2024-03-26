3192335af62954e69137f15dfabde121146223d5
Gokul <gokulchandan24@gmail.com>
2024-03-26 17:29:41 +0530

    app initialization

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
